e2bac88b3de8c46ec9d7f83e6bc906cdd228f7ab
Keynort <lukyanchuk.kirya@gmail.com>
2022-03-29 22:17:19 +0600

    ///

--- src/Solution1.java
+++ src/Solution1.java
@@ -2,5 +2,6 @@ public class Solution1 {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("Hello man");
+        System.out.println(90);
     }
 }
