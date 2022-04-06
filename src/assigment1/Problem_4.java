858d8443c8e9b2aa6952341e7ca6f1439d3bf6ca
Keynort <lukyanchuk.kirya@gmail.com>
2022-04-06 14:14:51 +0600

    Lukyanchuk Kirill

--- src/assigment1/Problem_4.java
+++ src/assigment1/Problem_4.java
@@ -1,12 +1,15 @@
+package assigment1;
+
 import java.util.Scanner;
 
-public class Problem_9 {
+public class Problem_4 {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n =in.nextInt();
-        int k =in.nextInt();
-        System.out.println(binomialCoefficient(n,k));
+        int n = in.nextInt();
+        System.out.println(factorial(n));
     }
+
     private static int factorial(int n) {
         int res = 1;
         if (n == 1 || n == 0) {
@@ -15,8 +18,4 @@ public class Problem_9 {
         res = n * factorial(n-1);
         return res;
     }
-    public static int binomialCoefficient(int n, int k){
-        return factorial(n) / (factorial(k) * factorial(n-k));
-    }
-
 }
